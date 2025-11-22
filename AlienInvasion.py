84bca7ea871ca6564b9cd2935e1173c183712054
Navkirat9339 <navkirat.singh20251@lpu.in>
2025-11-22 19:34:18 +0530

    AlienInvasion game creation.

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -10,10 +10,11 @@ class AlienInvasion:
         """Initialise the game, and create new game resource"""
         pygame.init()
         self.settings = Settings()
-        self.ship = Ship()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """start the main loop of the game"""
