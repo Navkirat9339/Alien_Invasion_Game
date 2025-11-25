35dbb0f8e2ccd16a48ac99e5e99a12ad78e89e5f
Navkirat9339 <navkirat.singh20251@lpu.in>
2025-11-25 14:46:17 +0530

    AlienInvasion game creation.

--- bullet.py
+++ bullet.py
@@ -23,7 +23,7 @@ class Bullet(Sprite):
         """Move the bullet up the screen"""
         #Update the decimal position of the bullet.
         self.y -= self.settings.bullet_speed
-        #Update the rect psotion.
+        #Update the rect position.
         self.rect.y = self.y
 
     def draw_bullet(self):
